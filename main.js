89af741bd1cd59aaa45015289025df9ab23d3797
netfloof <116656626+netfloof@users.noreply.github.com>
2022-10-27 10:10:05 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function protoHub() {
+  location.replace("https://protohub.online/account.php?v1=hertztheproto")
+}
+
+function twitter() {
+  location.replace("https://twitter.com/hertztheproto")
+}
